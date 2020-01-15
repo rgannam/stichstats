13966344bee9bdb15a8210e2d127db5c13fc1ba6
Rebecca Gannam <rebeccagannam@gmail.com>
2020-01-14 22:37:35 -0600

    test(pattern-list): unit test for computer pattern document property

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Input from "@/components/Input.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -8,5 +9,19 @@ describe("HelloWorld.vue", () => {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("Input.vue", () => {
+  it("combines name and designer into a document", () => {
+    const name = 'test123';
+    const designer = 'test456';
+    const wrapper = shallowMount(Input, {
+      propsData: {
+        patternName: name,
+        patternDesigner: designer
+      }
+    });
+    expect((<any>wrapper.vm).patternDocument).toEqual({name: 'test123', designer: 'test456'});
   });
 });
